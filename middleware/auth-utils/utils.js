ec5b0ef21a51d4fc716b7373c299386fa1af0576
Kevin Arthur <kevin@outlier.ai>
2019-12-23 14:38:36 -0800

    remove realm adapter util

--- middleware/auth-utils/utils.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function getAdapterForRealm (request, keycloakAdapters) {
-  return request.session.realmInfo && request.session.realmInfo.name ?
-    keycloakAdapters[request.session.realmInfo.name] :
-    keycloakAdapters['Default-Realm'];
-}
-
-module.exports.getAdapterForRealm = getAdapterForRealm;
